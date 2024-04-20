26add340ee81da0cd41d07286e282d594352e237
kooji00github <157189468+kooji00github@users.noreply.github.com>
2024-04-20 10:59:28 +0900

    ページネーションのビューを修正

--- resources/views/item/index.blade.php
+++ resources/views/item/index.blade.php
@@ -12,7 +12,7 @@
             <div class="card">
                 <div class="card-header">
                     <div class="row">
-                        <div class="col-2">
+                        <div class="col-2 d-flex align-items-center">
                             <h3 class="card-title">商品一覧</h3>
                         </div>
                         <div class="col-8">
@@ -25,7 +25,7 @@
                                             <option value="衛生用品" {{ $type == '衛生用品' ? 'selected' : '' }}>衛生用品</option>
                                             <option value="衣料品" {{ $type == '衣料品' ? 'selected' : '' }}>衣料品</option>
                                             <option value="医療品" {{ $type == '医療品' ? 'selected' : '' }}>医療品</option>
-                                            <option value="情報機器" {{ $type == '情報機器' ? 'selected' : '' }}>情報機器</option>
+                                            <option value="電子機器" {{ $type == '電子機器' ? 'selected' : '' }}>電子機器</option>
                                             <option value="その他" {{ $type == 'その他' ? 'selected' : '' }}>その他</option>
                                         </select>
                                     </div>
@@ -34,15 +34,15 @@
                                     </div>
                                     <div class="col-3 px-0">
                                         <div class="input-group-append">
-                                            <button type="submit" class="btn btn-default"><i class="fas fa-search"></i></button>
-                                            <button type="button" id="clearSearch" class="btn btn-default"><i class="fas fa-times"></i></button>
+                                            <button type="submit" class="btn btn-success mx-1"><i class="fas fa-search"></i> 絞り込み</button>
+                                            <button type="button" id="clearSearch" class="btn btn-default"><i class="fas fa-times"></i>リセット</button>
                                         </div>
                                     </div>
                                 </div>
                             </form>
                         </div>
                         <div class="col-2 d-flex justify-content-end">
-                            <a href="{{ url('items/add') }}" class="btn btn-default">商品登録</a>
+                            <a href="{{ url('items/add') }}" class="btn btn-default"><i class="fas fa-plus"></i> 商品登録</a>
                         </div>
                     </div>
                 </div>
@@ -68,9 +68,14 @@
                         </tbody>
                     </table>
                 </div>
-                <div class="d-flex justify-content-center mt-2">
-                    <!-- ページネーションを配置 -->
-                    {{ $items->appends(['keyword' => $keyword, 'type' => $type])->links('pagination::bootstrap-4') }}
+                <div class="d-flex justify-content-between mx-3 mt-2">
+                    <div>
+                        {{ $items->firstItem() }} - {{ $items->lastItem() }} / {{ $items->total() }}
+                    </div>
+                    <div>
+                        <!-- ページネーションを配置 -->
+                        {{ $items->appends(['keyword' => $keyword, 'type' => $type])->links('pagination::custom') }}
+                    </div>
                 </div>
             </div>
         </div>
