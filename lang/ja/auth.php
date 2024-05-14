1d151aaa9cd33a3cd0f5ca652d36bdc56c6b08ae
kooji00github <157189468+kooji00github@users.noreply.github.com>
2024-05-15 07:54:33 +0900

    日本語化コメント変更

--- lang/ja/auth.php
+++ lang/ja/auth.php
@@ -1,6 +1,9 @@
 <?php
 
-return [
-    'failed' => 'これらの資格情報はデーターベースの記録と一致しません。',
+declare(strict_types=1);
 
+return [
+    'failed'   => '認証に失敗しました。',
+    'password' => 'パスワードが正しくありません。',
+    'throttle' => 'ログインの試行回数が多すぎます。:seconds 秒後にお試しください。',
 ];
--- lang/ja/passwords.php
+++ lang/ja/passwords.php
@@ -1,22 +1,11 @@
 <?php
 
+declare(strict_types=1);
+
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Password Reset Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines are the default lines which match reasons
-    | that are given by the password broker for a password update attempt
-    | has failed, such as for an invalid token or invalid new password.
-    |
-    */
-
-    'reset' => 'パスワードがリセットされました。',
-    'sent' => 'パスワードリセットリンクをメールで送信しました。',
-    'throttled' => '試行する前に少し待ってください。',
-    'token' => 'このパスワードリセットトークンは無効です。',
-    'user' => "そのメールアドレスのユーザーを見つけることができません。",
-
+    'reset'     => 'パスワードが再設定されました。',
+    'sent'      => 'パスワードリセットメールを送信しました。',
+    'throttled' => '時間を置いて再度お試しください。',
+    'token'     => 'このパスワード再設定トークンは無効です。',
+    'user'      => 'このメールアドレスに一致するユーザーがいません。',
 ];
